0d3d6dd8904a15cbbb8b85cf025291b1d190cdd7
kiu_elchibegashvili.zurab <elchibegashvili.zurab@kiu.edu.ge>
2021-12-07 16:21:00 +0400

    .

---
+++ src/pgdp/collections/DataStructureLink.java
@@ -0,0 +1,27 @@
+package pgdp.collections;
+
+public class DataStructureLink<T> {
+
+    private DataStructureConnector<T> firstConn, secondConn;
+
+    public DataStructureLink(DataStructureConnector<T> firtsConn, DataStructureConnector<T> secondConn){
+        this.firstConn = firtsConn;
+        this.secondConn = secondConn;
+    }
+
+    public boolean moveNextFromAToB() {
+        if(firstConn.hasNextElement()){
+            firstConn.removeNextElement();
+            secondConn.addElement(firstConn.removeNextElement());
+            return true;
+        }
+        else return false;
+    }
+
+    public void moveAllFromAToB() {
+        while(firstConn.hasNextElement()){
+            firstConn.removeNextElement();
+            secondConn.addElement(firstConn.removeNextElement());
+        }
+    }
+}
