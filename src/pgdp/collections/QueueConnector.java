cde09a84afaee14faa8d439b48fb7d0ecccd0320
kiu_elchibegashvili.zurab <elchibegashvili.zurab@kiu.edu.ge>
2021-12-07 14:49:50 +0400

    .

--- src/pgdp/collections/QueueConnector.java
+++ src/pgdp/collections/QueueConnector.java
@@ -1,33 +1,33 @@
-//package pgdp.collections;
-//
-//public class QueueConnector<T> implements DataStructureConnector<T> {
-//
-//    private Queue<T> queue;
-//
-//    public QueueConnector(LinkedQueue<T> queue) {
-//        this.queue = queue;
-//    }
-//
+package pgdp.collections;
+
+public class QueueConnector<T> implements DataStructureConnector<T> {
+
+    private Queue<T> queue;
+
+    public QueueConnector(LinkedQueue<T> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public boolean hasNextElement() {
+        return (!queue.isEmpty());
+    }
+
+    @Override
+    public void addElement(T element) {
+//        queue = new LinkedQueue<T>();
+//        queue.enqueue(element);
+    }
+
+    @Override
+    public T removeNextElement() {
+        return queue.dequeue();
+    }
+
 //    @Override
-//    public boolean hasNextElement() {
-//        return (!queue.isEmpty());
+//    public String toString() {
+//        return "QueueConnector{" +
+//                "queue=" + queue +
+//                '}';
 //    }
-//
-//    @Override
-//    public void addElement(T element) {
-////        queue = new LinkedQueue<T>();
-////        queue.enqueue(element);
-//    }
-//
-//    @Override
-//    public T removeNextElement() {
-//        return queue.dequeue();
-//    }
-//
-////    @Override
-////    public String toString() {
-////        return "QueueConnector{" +
-////                "queue=" + queue +
-////                '}';
-////    }
-//}
+}
