99b0bf1e105451ab833dcfcdc0884194de8075a0
kiu_elchibegashvili.zurab <elchibegashvili.zurab@kiu.edu.ge>
2021-12-07 14:46:08 +0400

    .

--- src/pgdp/collections/LinkedStack.java
+++ src/pgdp/collections/LinkedStack.java
@@ -1,51 +1,51 @@
-//package pgdp.collections;
-//
-//public class LinkedStack<T> implements Stack<T>{
-//
-//    private List<T> next;
-//
-//    public LinkedStack() {
-//        next = null;
+package pgdp.collections;
+
+public class LinkedStack<T> implements Stack<T>{
+
+    private List<T> next;
+
+    public LinkedStack() {
+        next = null;
+    }
+
+    public LinkedStack(List<T> next){
+        this.next = next;
+    }
+
+    @Override
+    public int size() {
+        int result = 0;
+        for(List<T> t=next; t!=null; t=t.getNext())
+            result++;
+        return result;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (next==null);
+    }
+
+    @Override
+    public void push(T bolo) {
+        next = new List<T>(bolo,next);
+    }
+
+    @Override
+    public T pop() {
+        if(next==null) return null;
+        T result = next.getInfo();
+        next = next.getNext();
+        return result;
+    }
+
+//    public String toString(){
+//        return next.toString();
 //    }
-//
-//    public LinkedStack(List<T> next){
-//        this.next = next;
-//    }
-//
+
 //    @Override
-//    public int size() {
-//        int result = 0;
-//        for(List<T> t=next; t!=null; t=t.getNext())
-//            result++;
-//        return result;
+//    public String toString() {
+//        return "LinkedStack{" +
+//                "next=" + next +
+//                '}';
 //    }
-//
-//    @Override
-//    public boolean isEmpty() {
-//        return (next==null);
-//    }
-//
-//    @Override
-//    public void push(T bolo) {
-//        next = new List<T>(bolo,next);
-//    }
-//
-//    @Override
-//    public T pop() {
-//        if(next==null) return null;
-//        T result = next.getInfo();
-//        next = next.getNext();
-//        return result;
-//    }
-//
-////    public String toString(){
-////        return next.toString();
-////    }
-//
-////    @Override
-////    public String toString() {
-////        return "LinkedStack{" +
-////                "next=" + next +
-////                '}';
-////    }
-//}
+}
