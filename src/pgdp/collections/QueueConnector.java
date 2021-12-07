b27a9106b9fa27536797942dfe01f0e2cee83e0f
kiu_elchibegashvili.zurab <elchibegashvili.zurab@kiu.edu.ge>
2021-12-07 15:15:16 +0400

    .

--- src/pgdp/collections/QueueConnector.java
+++ src/pgdp/collections/QueueConnector.java
@@ -1,32 +1,32 @@
-package pgdp.collections;
-
-public class QueueConnector<T> implements DataStructureConnector<T> {
-
-    private Queue<T> queue;
-
-    public QueueConnector(Queue<T> queue) {
-        this.queue = queue;
-    }
-
-    @Override
-    public boolean hasNextElement() {
-        return (!queue.isEmpty());
-    }
-
-    @Override
-    public void addElement(T element) {
-        queue.enqueue(element);
-    }
-
-    @Override
-    public T removeNextElement() {
-        return queue.dequeue();
-    }
-
-//    @Override
-//    public String toString() {
-//        return "QueueConnector{" +
-//                "queue=" + queue +
-//                '}';
+//package pgdp.collections;
+//
+//public class QueueConnector<T> implements DataStructureConnector<T> {
+//
+//    private Queue<T> queue;
+//
+//    public QueueConnector(Queue<T> queue) {
+//        this.queue = queue;
 //    }
-}
+//
+//    @Override
+//    public boolean hasNextElement() {
+//        return (!queue.isEmpty());
+//    }
+//
+//    @Override
+//    public void addElement(T element) {
+//        queue.enqueue(element);
+//    }
+//
+//    @Override
+//    public T removeNextElement() {
+//        return queue.dequeue();
+//    }
+//
+////    @Override
+////    public String toString() {
+////        return "QueueConnector{" +
+////                "queue=" + queue +
+////                '}';
+////    }
+//}
