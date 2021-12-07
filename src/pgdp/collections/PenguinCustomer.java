959bb2f151760ec682caf3895932ba2c6a1da7eb
kiu_elchibegashvili.zurab <elchibegashvili.zurab@kiu.edu.ge>
2021-12-07 19:19:31 +0400

    .

--- src/pgdp/collections/PenguinCustomer.java
+++ src/pgdp/collections/PenguinCustomer.java
@@ -3,57 +3,57 @@ package pgdp.collections;
 public class PenguinCustomer {
 
     private final String name;
-    private int cash;
-    private final Stack<FishyProduct> product;
+    private int money;
+    private final Stack<FishyProduct> products;
 
     public PenguinCustomer(String name, int initialMoney) {
         this.name = name;
-        this.cash = initialMoney;
-        if(this.name==null || this.cash<0) ExceptionUtil.illegalArgument("penguins cannot have debts");
-        this.product = new LinkedStack<>();
+        this.money = initialMoney;
+        if(this.name==null || this.money <0) ExceptionUtil.illegalArgument("penguins cannot have debts");
+        this.products = new LinkedStack<>();
     }
 
     public String getName() {
         return name;
     }
 
-    public int getCash() {
-        return cash;
+    public int getMoney() {
+        return money;
     }
 
     public Stack<FishyProduct> getProduct() {
-        return product;
+        return products;
     }
 
     public void addProductToBasket(FishyProduct newProduct){
-        product.push(newProduct);
+        products.push(newProduct);
     }
 
     public void placeAllProductsOnBand(Queue<FishyProduct> allProducts) {
-        StackConnector<FishyProduct> produqtebi = new StackConnector<>(product);
+        StackConnector<FishyProduct> produqtebi = new StackConnector<>(products);
         QueueConnector<FishyProduct> daxli = new QueueConnector<>(allProducts);
         DataStructureLink<FishyProduct> sayideli = new DataStructureLink<>(produqtebi,daxli);
         sayideli.moveAllFromAToB();
     }
 
     public void takeAllProductsFromBand(Queue<FishyProduct> allProducts) {
-        StackConnector<FishyProduct> produqtebi = new StackConnector<>(product);
+        StackConnector<FishyProduct> produqtebi = new StackConnector<>(products);
         QueueConnector<FishyProduct> daxli = new QueueConnector<>(allProducts);
         DataStructureLink<FishyProduct> nayidi = new DataStructureLink<>(daxli,produqtebi);
         nayidi.moveAllFromAToB();
     }
 
     public void pay(int value) {
-        if(value<0 || cash< value) throw new IllegalArgumentException();
-        cash -= value;
+        if(value<0 || money < value) throw new IllegalArgumentException();
+        money -= value;
     }
 
     @Override
     public String toString() {
         return "PenguinCustomer{" +
                 "name='" + name + '\'' +
-                ", cash=" + cash +
-                ", product=" + product +
+                ", cash=" + money +
+                ", product=" + products +
                 '}';
     }
 }
