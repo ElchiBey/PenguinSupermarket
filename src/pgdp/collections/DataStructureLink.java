5dbd06b3eacecf9b476168e3fe11dc4f805929f5
ElchiBey <111178447+ElchiBey@users.noreply.github.com>
2022-08-18 20:45:05 +0400

    Update DataStructureLink.java

--- src/pgdp/collections/DataStructureLink.java
+++ src/pgdp/collections/DataStructureLink.java
@@ -4,21 +4,20 @@ public class DataStructureLink<T> {
 
     private DataStructureConnector<T> firstConn, secondConn;
 
-    public DataStructureLink(DataStructureConnector<T> firtsConn, DataStructureConnector<T> secondConn){
-        this.firstConn = firtsConn;
+    public DataStructureLink(DataStructureConnector<T> firstConn, DataStructureConnector<T> secondConn) {
+        this.firstConn = firstConn;
         this.secondConn = secondConn;
     }
 
     public boolean moveNextFromAToB() {
-        if(firstConn.hasNextElement()){
+        if (firstConn.hasNextElement()) {
             secondConn.addElement(firstConn.removeNextElement());
             return true;
-        }
-        else return false;
+        } else return false;
     }
 
     public void moveAllFromAToB() {
-        while(firstConn.hasNextElement()){
+        while (firstConn.hasNextElement()) {
             secondConn.addElement(firstConn.removeNextElement());
         }
     }
