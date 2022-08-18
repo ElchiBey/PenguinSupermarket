f35245d1aa0c67958d8fcab5da6ec34a8b831936
ElchiBey <111178447+ElchiBey@users.noreply.github.com>
2022-08-18 20:50:06 +0400

    Update PenguinSupermarket.java

--- src/pgdp/collections/PenguinSupermarket.java
+++ src/pgdp/collections/PenguinSupermarket.java
@@ -2,12 +2,12 @@ package pgdp.collections;
 
 public class PenguinSupermarket {
 
-    private Checkout[] checkouts;
+    private final Checkout[] checkouts;
 
     public PenguinSupermarket(int amount) {
-        if(amount < 0) ExceptionUtil.illegalArgument("must be greater than 0");
+        if (amount < 0) ExceptionUtil.illegalArgument("must be greater than 0");
         checkouts = new Checkout[amount];
-        for(int i=0; i<amount; i++){
+        for (int i = 0; i < amount; i++) {
             checkouts[i] = new Checkout();
         }
     }
@@ -16,28 +16,39 @@ public class PenguinSupermarket {
         return checkouts;
     }
 
-    public Checkout[] getCheckoutWithSmallestQueue(){
+    public Checkout[] getCheckoutWithSmallestQueue() {
         int result = Integer.MAX_VALUE;
-        int j=0;
-        for(int i=0; i<checkouts.length; i++){
-            if(checkouts[i].queueLength() <= result)
-                j=i;
+        int j = 0;
+        for (int i = 0; i < checkouts.length; i++) {
+            if (checkouts[i].queueLength() <= result)
+                j = i;
             result = checkouts[i].queueLength();
         }
         return new Checkout[]{checkouts[j]};
     }
 
-    public void closeCheckout(int index){
-        if(checkouts.length==1)  ExceptionUtil.illegalArgument("magazia jer ar daketila, sad midixar?!");
-        for(int i=0; i<checkouts.length; i++){
-            if(checkouts[i]==null) ExceptionUtil.illegalArgument("viketebiiiit! <3");
-            checkouts[i]=null;
-        }
+    public void closeCheckout(int index) {
+        if (checkouts.length == 1) ExceptionUtil.illegalArgument("magazia jer ar daketila, sad midixar?!");
+        if (checkouts[index] == null) ExceptionUtil.illegalArgument("egeti salaro ar gvaaqvs");
+
+        QueueConnector<PenguinCustomer> sameQueue = new QueueConnector<>(checkouts[index].getQueue());
+        QueueConnector<PenguinCustomer> emptyQueue = new QueueConnector<>(null);
+        DataStructureLink<PenguinCustomer> data = new DataStructureLink<>(sameQueue, emptyQueue);
+        data.moveAllFromAToB();
+        Queue<PenguinCustomer> reversedQueue = (Queue<PenguinCustomer>) data;
+        reversedQueue.dequeue().goToCheckout(this);
+
+        checkouts[index] = null;
+
     }
 
-    public void serveCustomers(){
-        for(int i=0; i< checkouts.length; i++){
-            checkouts[i].serveNextCustomer();
+
+    public void serveCustomers() {
+        for (Checkout checkout : checkouts) {
+            checkout.serveNextCustomer();
         }
+//        for(int i = 0; i < checkouts.length; i++) {
+//            checkouts[i].serveNextCustomer();
+//        }
     }
 }
