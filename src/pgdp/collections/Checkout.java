58fe51401e7a1514af7e7586a0fa16baa55218e4
ElchiBey <111178447+ElchiBey@users.noreply.github.com>
2022-08-18 20:43:19 +0400

    Update Checkout.java

--- src/pgdp/collections/Checkout.java
+++ src/pgdp/collections/Checkout.java
@@ -2,9 +2,9 @@ package pgdp.collections;
 
 public class Checkout {
 
-    private Queue<PenguinCustomer> queue;
-    private Queue<FishyProduct> bandBeforeCashier;
-    private Queue<FishyProduct> bandAfterCashier;
+    private final Queue<PenguinCustomer> queue;
+    private final Queue<FishyProduct> bandBeforeCashier;
+    private final Queue<FishyProduct> bandAfterCashier;
 
     public Checkout() {
         this.queue = new LinkedQueue<>();
@@ -29,15 +29,15 @@ public class Checkout {
     }
 
     public void serveNextCustomer() {
-        while (queue != null) {
+        while (queue.dequeue() != null) {
             int cost = 0;
             bandBeforeCashier.enqueue((FishyProduct) queue.dequeue().getProducts());
-            while(bandBeforeCashier.dequeue()!=null) {
+            while (bandBeforeCashier.dequeue() != null) {
                 bandAfterCashier.enqueue(bandBeforeCashier.dequeue());
                 cost += bandAfterCashier.dequeue().getPrice();
             }
             queue.dequeue().takeAllProductsFromBand((bandAfterCashier));
-            if(queue.dequeue().getMoney()<cost) ExceptionUtil.illegalArgument("penguins cannot have debts");
+            if (queue.dequeue().getMoney() < cost) ExceptionUtil.illegalArgument("penguins cannot have debts");
             queue.dequeue().pay(cost);
             queue.dequeue();
         }
@@ -46,9 +46,9 @@ public class Checkout {
     @Override
     public String toString() {
         return "Checkout{" +
-                "queue=" + queue +
-                ", bandBeforeCashier=" + bandBeforeCashier +
-                ", bandAfterCashier=" + bandAfterCashier +
+                "queue = " + queue +
+                ", bandBeforeCashier = " + bandBeforeCashier +
+                ", bandAfterCashier = " + bandAfterCashier +
                 '}';
     }
 }
